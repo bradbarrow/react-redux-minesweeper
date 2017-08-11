cd1c007c24023c35eaf7624f1ae62fc769e8db8c
bradbarrow <bradbarrow@gmail.com>
2017-08-11 16:31:24 +1000

    Dee + Brad

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const field = [
+const before = [
+  ['?', '?', '?', '?', '?'],
+  ['?', '?', '?', '?', '?'],
+  ['?', '?', '?', '?', '?'],
+  ['?', '?', '?', '?', '?'],
+  ['?', '?', '?', '?', '?'],
+];
+
+const after = [
   [1, 1, 1, 0, 0],
   [1, -1, 2, 1, 0],
   [1, 2, -1, 1, 0],
@@ -11,51 +19,56 @@ const field = [
 ];
 
 class Cell extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      revealed: false,
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState({
-      revealed: true,
-    });
-  }
-
   render() {
-      const { revealed } = this.state;
-      const { value } = this.props;
+      // const { revealed } = this.state;
+      // const { value } = this.props;
 
       return (
-        <span onClick={this.handleClick}>{ revealed ? value : '?' }</span>
+        <span onClick={this.props.handleClick}></span>
       );
   }
 }
 
 const Field = ({ field }) => (
   <div>
-   { field.map(row => (
+   { field.map((row, y) => (
     <div>
       {
-        row.map(value => (
-          <Cell value={value} />
+        row.map((value, x) => (
+          <Cell value={value} onClick={() => {
+            this.props.handleClick(x, y)
+          }} />
         ))
       }
     </div>
   )) }
   </div>
-)
+);
 
 class Game extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      before: before,
+      after: after,
+      current: before,
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick({ x, y }) {
+    console.log('x,y', x, y);
+    // this.setState({
+    //
+    // })
+  }
+
   render() {
     return (
       <div>
-        <Field field={field}/>
+        <Field field={this.state.current} onClick={this.handleClick} />
       </div>
     );
   }
